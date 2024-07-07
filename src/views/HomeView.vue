f4a8237401a4dbf752840ff83aabf0c267d28dff
JeewanthaSenanayake <jeewanthamsenanayake@gmail.com>
2024-07-07 23:27:29 +0530

    home\

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the Home page!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomeView'
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
