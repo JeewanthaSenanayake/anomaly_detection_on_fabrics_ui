a50a3d16ebeb4846362a64defbd0395a9ffef951
JeewanthaSenanayake <jeewanthamsenanayake@gmail.com>
2024-07-08 11:56:59 +0530

    defect test done

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container class="ma-3">
+    <v-container>
         <H1 class="text-center">Fabric Defect Detector</H1>
         <v-card class="my-5 mx-5" elevation="2" dark>
             <v-card-title>
@@ -8,6 +8,28 @@
 
             <div justify="center" align="center">
                 <v-img v-if="selectedImage" :src="imgeUrl" height="300px" width="300px"></v-img>
+                <div v-if="responseData">
+                    <v-col>
+                        <v-col>
+                            {{ responseData.file }}
+                        </v-col>
+                        <v-col>
+                            {{ dateTime(responseData.date) }}
+                        </v-col>
+                        <v-col>
+                            <v-chip v-if="responseData.status" color="error" class="ma-1 pa-2" dark>
+                                Defective
+                            </v-chip>
+                            <v-chip v-else color="success" class="ma-1 pa-2" dark>
+                                Not Defective
+                            </v-chip>
+                        </v-col>
+                    </v-col>
+                </div>
+                <div v-else>
+                    <v-progress-linear v-if="loading" class="mt-4" indeterminate
+                        color="yellow darken-2"></v-progress-linear>
+                </div>
             </div>
 
             <v-card-text>
@@ -25,11 +47,14 @@
 </template>
 
 <script>
+import axios from '@/service/axiosConfig';
 export default {
     name: 'HomeComponent',
 
     data: () => ({
         selectedImage: null,
+        responseData: null,
+        loading: false,
         rules: [
             value => !!value || "File required",
         ],
@@ -37,16 +62,55 @@ export default {
 
     methods: {
         // Add your methods here
-        submit() {
-            console.log("Image submitted");
+
+        async submit() {
+            this.loading = true;
+            const formData = new FormData()
+            formData.append('file', this.selectedImage);
+            await axios.post('/fabric/api/v1/isdefective', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }).then((response) => {
+                this.responseData = response.data;
+                this.loading = false;
+                console.log(response.data);
+            }).catch((error) => {
+                console.log(error);
+                this.loading = false;
+            });
+        },
+
+        dateTime(d) {
+
+            const date = new Date(d);
+            const options = {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            };
+
+            return date.toLocaleDateString('en-US', options);
+
         },
     },
+
 
     computed: {
         // Add your computed properties here
         imgeUrl() {
             return URL.createObjectURL(this.selectedImage);
         },
+    },
+
+    watch: {
+        selectedImage() {
+            this.responseData = null;
+            this.loading = false;
+        }
     },
 };
 </script>
